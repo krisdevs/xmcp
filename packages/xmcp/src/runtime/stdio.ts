92a79aee7700a99430585f860595aff2e07eed21
valebearzotti <valebearzotti1@gmail.com>
2025-05-17 20:19:15 -0300

    remove extra assertion

--- packages/xmcp/src/runtime/stdio.ts
+++ packages/xmcp/src/runtime/stdio.ts
@@ -2,7 +2,7 @@ import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js"
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 
 // @ts-expect-error: injected by compiler
-const mcpServer = INJECTED_MCP_SERVER as any as McpServer
+const mcpServer = INJECTED_MCP_SERVER as McpServer
 
 const transport = new StdioServerTransport();
 mcpServer.connect(transport);
