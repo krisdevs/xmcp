9ac6f48f17067e734e8b176fa8d441f9b452e5b0
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2025-06-29 20:30:46 -0300

    update examples

--- examples/with-config-ts/xmcp.config.ts
+++ examples/with-config-ts/xmcp.config.ts
@@ -1,6 +1,12 @@
-const config = {
+import { XmcpConfig } from "xmcp";
+
+const config: XmcpConfig = {
   http: {
     port: 3002,
+  },
+  webpack: (config) => {
+    console.log("webpack config", config);
+    return config;
   },
 };
 
