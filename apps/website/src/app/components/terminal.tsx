30b11776fde05c6a14b3dff199886bedbc5a55ea
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2025-06-30 20:55:38 -0300

    add terminal

--- apps/website/src/app/components/terminal.tsx
+++ apps/website/src/app/components/terminal.tsx
@@ -1,13 +1,29 @@
+import { cn } from "@/utils/cn";
 import styles from "./terminal.module.css";
 
 function TerminalContainer({ children }: { children: React.ReactNode }) {
   return (
-    <div className={styles.container}>
-      <div>{children}</div>
+    <div className={cn(styles.container, "p-1.5 relative")}>
+      <div className="p-4 bg-black border border-gray-400">{children}</div>
+      <div className="pointer-none absolute -top-1 left-4 flex gap-1.5 [&>div]:w-2 [&>div]:h-4 [&>div]:bg-black">
+        <div />
+        <div />
+        <div />
+        <div />
+      </div>
     </div>
   );
 }
 
 export function Terminal() {
-  return <TerminalContainer>Test</TerminalContainer>;
+  return (
+    <TerminalContainer>
+      <div className="aspect-video">
+        <div className="text-left flex gap-2 items-center leading-none">
+          <code>⊹ npx create-xmcp-app</code>
+          <div className="bg-white h-4 w-1.5 top-[1px] relative" />
+        </div>
+      </div>
+    </TerminalContainer>
+  );
 }
--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -2,8 +2,9 @@ import { Terminal } from "./components/terminal";
 
 export default function Home() {
   return (
-    <div className="font-[family-name:var(--font-geist-sans)]">
-      <div className="max-w-[40rem] mx-auto">
+    <div className="font-mono py-8">
+      <div className="max-w-[40rem] mx-auto px-2 text-center flex flex-col gap-8">
+        <h1 className="text-lg">xmcp_</h1>
         <Terminal />
       </div>
     </div>
