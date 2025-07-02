945841a57a0259f580c3c1a37a6cd0860bb67709
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2025-07-02 08:26:14 -0300

    fix lint

--- apps/website/src/app/components/render-texture/index.tsx
+++ apps/website/src/app/components/render-texture/index.tsx
@@ -1,20 +1,20 @@
-import { createPortal, RenderCallback, useFrame } from "@react-three/fiber"
-import { PropsWithChildren, RefObject, useMemo } from "react"
+import { saveGlState } from "@/utils/save-gl-state";
+import { createPortal, RenderCallback, useFrame } from "@react-three/fiber";
+import { PropsWithChildren, RefObject, useMemo } from "react";
 import {
   OrthographicCamera,
   PerspectiveCamera,
   Scene,
-  WebGLRenderTarget
-} from "three"
-import { saveGlState } from "@/lib/save-gl-state"
+  WebGLRenderTarget,
+} from "three";
 
 export interface RenderTextureProps {
-  renderTarget: WebGLRenderTarget | RefObject<WebGLRenderTarget> | null
-  beforeRender?: RenderCallback
-  afterRender?: RenderCallback
-  autoRender?: boolean
-  camera?: OrthographicCamera | PerspectiveCamera
-  priority?: number
+  renderTarget: WebGLRenderTarget | RefObject<WebGLRenderTarget> | null;
+  beforeRender?: RenderCallback;
+  afterRender?: RenderCallback;
+  autoRender?: boolean;
+  camera?: OrthographicCamera | PerspectiveCamera;
+  priority?: number;
 }
 
 function RenderTextureWrapper({
@@ -24,37 +24,35 @@ function RenderTextureWrapper({
   autoRender = true,
   camera,
   priority,
-  children
+  children,
 }: PropsWithChildren<RenderTextureProps>) {
   useFrame((state, delta) => {
-    // console.log(state.camera.position)
-
-    const restore = saveGlState(state)
+    const restore = saveGlState(state);
     if (beforeRender) {
-      beforeRender(state, delta)
+      beforeRender(state, delta);
     }
 
     if (autoRender) {
       if (renderTarget && "current" in renderTarget) {
-        state.gl.setRenderTarget(renderTarget.current)
+        state.gl.setRenderTarget(renderTarget.current);
       } else {
-        state.gl.setRenderTarget(renderTarget)
+        state.gl.setRenderTarget(renderTarget);
       }
 
-      state.gl.render(state.scene, camera ?? state.camera)
+      state.gl.render(state.scene, camera ?? state.camera);
     }
 
     if (afterRender) {
-      afterRender(state, delta)
+      afterRender(state, delta);
     }
-    restore()
-  }, priority)
+    restore();
+  }, priority);
 
-  return children
+  return children;
 }
 
 export function RenderTexture(props: PropsWithChildren<RenderTextureProps>) {
-  const containerScene = useMemo(() => new Scene(), [])
+  const containerScene = useMemo(() => new Scene(), []);
 
   return (
     <>
@@ -65,5 +63,5 @@ export function RenderTexture(props: PropsWithChildren<RenderTextureProps>) {
         containerScene
       )}
     </>
-  )
+  );
 }
