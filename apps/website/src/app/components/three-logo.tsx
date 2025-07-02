12de13ca1f3be721f1f3f08309efab88e97bd7c5
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2025-07-01 23:35:00 -0300

    shader base

--- apps/website/src/app/components/three-logo.tsx
+++ apps/website/src/app/components/three-logo.tsx
@@ -2,13 +2,16 @@
 
 import {
   PerspectiveCamera,
+  useFBO,
   useGLTF,
   useMatcapTexture,
 } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import { GLTF } from "three/examples/jsm/Addons.js";
 import * as THREE from "three";
+import { useShader } from "@/hook/use-shader";
+import { saveGlState } from "@/utils/save-gl-state";
 
 interface ThreeLogoProps {
   scale?: number;
@@ -35,6 +38,59 @@ function ThreeLogo({
   const { nodes } = useGLTF("/xmcp.glb") as any as GLTFResult;
 
   const [matcap] = useMatcapTexture("3B3C3F_DAD9D5_929290_ABACA8", 1024);
+
+  const revealTarget = useFBO(1024, 1024, { type: THREE.FloatType });
+
+  const revealShader = useShader(
+    {
+      vertexShader: /*glsl*/ `
+      void main() {
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      }
+    `,
+      fragmentShader: /*glsl*/ `
+      void main() {
+        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+      }
+    `,
+    },
+    {
+      uTime: { value: 0 },
+      uReveal: { value: 0 },
+    }
+  );
+
+  useFrame((state) => {
+    const restore = saveGlState(state);
+
+    state.gl.setRenderTarget(revealTarget);
+    state.gl.clear();
+
+    restore();
+  });
+
+  const matcapMaterial = useShader(
+    {
+      vertexShader: /*glsl*/ `
+      void main() {
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      }
+    `,
+      fragmentShader: /*glsl*/ `
+
+      uniform sampler2D uMatcap;
+      uniform sampler2D uReveal;
+
+      void main() {
+        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+      }
+    `,
+    },
+    {
+      uReveal: { value: revealTarget },
+      uMatcap: { value: matcap },
+    }
+  );
 
   return (
     <group ref={groupRef} scale={scale} position={position} rotation={rotation}>
