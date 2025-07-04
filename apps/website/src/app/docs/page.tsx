9afbe9e249c8554aa9bca95c0695204940431e47
valebearzotti <valebearzotti1@gmail.com>
2025-07-04 18:29:37 -0300

    upd docs page

--- apps/website/src/app/docs/page.tsx
+++ apps/website/src/app/docs/page.tsx
@@ -1,7 +1,15 @@
 export default async function DocsPage() {
   return (
-    <div>
-      <h1>Docs</h1>
+    <div className="w-full mx-auto py-8">
+      <div className="prose prose-invert max-w-none">
+        <h1>Documentation</h1>
+        <h3>The framework for building & shipping MCP applications.</h3>
+        <p>
+          Designed with DX in mind, it streamlines development and lowers the
+          barrier to entry for anyone looking to create and deploy powerful
+          tools on top of the MCP ecosystem.
+        </p>
+      </div>
     </div>
   );
 }
