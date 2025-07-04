c0a9bd64c1ac7bef19292ddd85081aa5bbb17135
valebearzotti <valebearzotti1@gmail.com>
2025-07-04 19:12:09 -0300

    nits

--- apps/website/src/app/docs/page.tsx
+++ apps/website/src/app/docs/page.tsx
@@ -1,10 +1,10 @@
 export default async function DocsPage() {
   return (
-    <div className="w-full mx-auto py-8 h-[calc(100dvh-12rem)]">
-      <div className="prose prose-invert max-w-none">
-        <h1>Documentation</h1>
+    <div className="w-full mx-auto h-[calc(100dvh-12rem)]  pt-[4rem]">
+      <div className="prose prose-invert max-w-none font-mono">
+        <h1 className="text-[2rem] uppercase">Documentation</h1>
         <h3>The framework for building & shipping MCP applications.</h3>
-        <p>
+        <p className="text-sm text-[#999999]">
           Designed with DX in mind, it streamlines development and lowers the
           barrier to entry for anyone looking to create and deploy powerful
           tools on top of the MCP ecosystem.
