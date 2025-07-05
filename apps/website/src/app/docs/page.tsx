d4df18c64bcae7ba0066aef2e25422f6b8ba407a
valebearzotti <valebearzotti1@gmail.com>
2025-07-05 01:24:57 -0300

    first thoughts on getting started docs + tool syntax separate tutorial updated

--- apps/website/src/app/docs/page.tsx
+++ apps/website/src/app/docs/page.tsx
@@ -4,11 +4,12 @@ export default async function DocsPage() {
       <div className="prose prose-invert max-w-none font-mono">
         <h1 className="text-[2rem] uppercase">Documentation</h1>
         <h3>The framework for building & shipping MCP applications.</h3>
-        <p className="text-sm text-[#999999]">
+        <p>
           Designed with DX in mind, it streamlines development and lowers the
           barrier to entry for anyone looking to create and deploy powerful
           tools on top of the MCP ecosystem.
         </p>
+        <p>&quot;A tool built for building tools.&quot;</p>
       </div>
     </div>
   );
