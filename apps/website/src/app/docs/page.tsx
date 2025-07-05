18675b46237ae78b8fcedb2c1dad8796d43217cb
valebearzotti <valebearzotti1@gmail.com>
2025-07-05 01:30:42 -0300

    xmcp on docs

--- apps/website/src/app/docs/page.tsx
+++ apps/website/src/app/docs/page.tsx
@@ -3,7 +3,7 @@ export default async function DocsPage() {
     <div className="w-full mx-auto h-[calc(100dvh-12rem)] pt-[4rem]">
       <div className="prose prose-invert max-w-none font-mono">
         <h1 className="text-[2rem] uppercase">Documentation</h1>
-        <h3>The framework for building & shipping MCP applications.</h3>
+        <h3>xmcp - The framework for building & shipping MCP applications.</h3>
         <p>
           Designed with DX in mind, it streamlines development and lowers the
           barrier to entry for anyone looking to create and deploy powerful
