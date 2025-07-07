1b39f5ca0000b70529c5527c5b67f65a32b5594b
valebearzotti <valebearzotti1@gmail.com>
2025-07-06 21:30:39 -0300

    copy btn terminal

--- apps/website/src/components/terminal/terminal.tsx
+++ apps/website/src/components/terminal/terminal.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import { cn } from "@/utils/cn";
 import styles from "./terminal.module.css";
 import TypingEffect from "./typing-effect";
+import { useEffect, useState } from "react";
+import { CopyButton } from "../ui/copy-button";
 
 function TerminalContainer({
   children,
@@ -10,7 +14,7 @@ function TerminalContainer({
   className?: string;
 }) {
   return (
-    <div className={cn(styles.container, "p-1 relative", className)}>
+    <div className={cn(styles.container, "p-1 relative group", className)}>
       <div className="p-1 px-2 bg-black border border-gray-400">{children}</div>
       <div className="pointer-none absolute -top-1 left-4 flex gap-1.5 [&>div]:w-2 [&>div]:h-4 [&>div]:bg-black">
         <div />
@@ -29,11 +33,21 @@ export function Terminal({
   children: string;
   className?: string;
 }) {
+  const [codeText, setCodeText] = useState("");
+
+  useEffect(() => {
+    setCodeText(children);
+  }, [children]);
+
   return (
     <TerminalContainer className={className}>
       <div>
         <TypingEffect>{children}</TypingEffect>
       </div>
+      <CopyButton
+        text={codeText}
+        className="absolute top-4 right-6 opacity-0 group-hover:opacity-100 transition-opacity duration-200"
+      />
     </TerminalContainer>
   );
 }
