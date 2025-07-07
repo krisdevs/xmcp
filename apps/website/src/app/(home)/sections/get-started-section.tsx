5f6ff42e2617b80ecda1ac541fa690b33188fc82
valebearzotti <valebearzotti1@gmail.com>
2025-07-06 22:13:08 -0300

    get started boxes

--- apps/website/src/app/(home)/sections/get-started-section.tsx
+++ apps/website/src/app/(home)/sections/get-started-section.tsx
@@ -6,21 +6,25 @@ const cards = [
     title: "Create an xmcp app",
     description: "Learn how to create an xmcp app with our documentation",
     href: "/docs#create-a-new-xmcp-app",
+    external: false,
   },
   {
     title: "Browse examples",
     description: "Learn more about xmcp by browsing our examples",
     href: "https://github.com/basementstudio/xmcp/tree/main/examples",
+    external: true,
   },
   {
     title: "Next.js",
     description: "Plug an xmcp app to an existing Next.js project",
     href: "/docs#usage-with-nextjs",
+    external: false,
   },
   {
     title: "Express",
     description: "Plug an xmcp app to an existing Express project",
     href: "/docs#usage-with-express",
+    external: false,
   },
 ];
 
@@ -42,11 +46,13 @@ function GetStartedCard({
   title,
   description,
   href,
+  external,
 }: {
   className?: string;
   title: string;
   description: string;
   href: string;
+  external: boolean;
 }) {
   return (
     <>
@@ -56,6 +62,8 @@ function GetStartedCard({
           "block text-left group relative overflow-visible h-full",
           className
         )}
+        target={external ? "_blank" : undefined}
+        rel={external ? "noopener noreferrer" : undefined}
       >
         <div
           className="top-1 left-1 absolute w-full h-full group-hover:border group-hover:visible invisible"
