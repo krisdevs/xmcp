efbb17e24b2b5f15b054bfd4147d6d51dcff010e
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2025-07-07 09:30:36 -0300

    fix codeblock font

--- apps/website/src/components/markdown/code-blocks.tsx
+++ apps/website/src/components/markdown/code-blocks.tsx
@@ -98,7 +98,7 @@ export function Pre({
     <pre
       ref={preRef}
       className={cn(
-        "my-8 border relative w-auto overflow-x-auto bg-black p-4",
+        "my-8 border relative w-auto overflow-x-auto bg-black p-4 [&_span]:!font-mono",
         className
       )}
       style={{ borderColor: "#333" }}
